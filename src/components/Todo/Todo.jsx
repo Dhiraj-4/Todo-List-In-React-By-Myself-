f94705749cca0fe1d63220190e7737812550d310
Dhiraj Londhe <dhirajdipaklondhe4@gmail.com>
2025-02-01 22:14:46 +0530

    Added some Features

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -19,13 +19,15 @@ function Todo({ todoData, onEdit, isFinished, onFinished, onDel}) {
             {(isEdit) ? <input type="text" 
                 onChange={(e) => setChangedData(e.target.value)}
                 placeholder="Enter Changed Todo Here..."
+                value={changedData}
                 className="outline-none border-4 rounded border-blue-500 px-3 py-2 text-base font-bold"
             />
-            : <span className="text-lg font-bold px-2">{todoData}</span>}
+            : (isFinished) ? <span className="text-lg font-bold px-2"><s className="decoration-2">{todoData}</s></span> : <span className="text-lg font-bold px-2">{todoData}</span>}
 
             <Button onClickHanlder={() => {
                 (isEdit) ? ((changedData) ? onEdit(changedData) : alert("Please Enter a Todo")) :'';
                 setIsEdit(!isEdit);
+                setChangedData(todoData);
             }} text={(isEdit) ? "Save": "Edit"} />
 
             <Button onClickHanlder={() => {
